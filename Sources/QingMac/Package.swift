bae9fb10584379cccf4228b312c3cbc1cbcb960e
DBK <cavanaughrosalinda455@gmail.com>
2026-09-25 22:23:19 +0800

    Delete Sources/QingMac/Package.swift

--- Sources/QingMac/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version: 6.0
-import PackageDescription
-
-let package = Package(
-    name: "QingMac",
-    platforms: [.macOS(.v14)],
-    products: [.executable(name: "QingMac", targets: ["QingMac"])],
-    targets: [.executableTarget(name: "QingMac", path: "Sources/QingMac")]
-)
